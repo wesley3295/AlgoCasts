b6c0fcd88ed6d8928dde0e83a50f50c56573736d
Wesley <wspencer3295@yahoo.com>
2021-07-15 13:57:36 -0700

    reverse string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//1st solution
+// function reverse(str) {
+//     //split string into array
+//     // reverse the array
+//     // join array into string
+//   return str.split("").reverse().join("");
+// }
+
+//2nd solution
+// function reverse(str) {
+//    //make variable 'reversed' an empty string
+//  // loop through string and + character to reversed variable =ing to reversed variable
+//   
+//let reversed = "";
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// }
+
+//3rd solution
+function reverse(str) {
+// split str into array
+// reduce setting cb to reversedStr + character, setting the accumulator in reduce to empty string.
+  return str.split("").reduce((reversedStr, char) => char + reversedStr, "");
+}
 
 module.exports = reverse;
