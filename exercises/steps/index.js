3d79060690d994e94d8f616811b451989a997e42
Wesley <wspencer3295@yahoo.com>
2021-07-24 23:15:17 -0700

    steps solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -16,7 +16,37 @@
 //       '##  '
 //       '### '
 //       '####'
+//1st solution
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column <= row) {
+//         stair += "#";
+//       } else {
+//         stair += " ";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// }
 
-function steps(n) {}
+//2nd solution
+function steps(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  steps(n, row, stair);
+}
 
 module.exports = steps;
